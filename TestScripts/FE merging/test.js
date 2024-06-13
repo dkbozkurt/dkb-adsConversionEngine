a9530689023b10742b0dc4354cc22bb5716c481c
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-06-13 14:36:00 +0200

    fe merging - python removed

--- TestScripts/FE merging/test.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "<p>This content is inserted using JavaScript!</p>";
-});
